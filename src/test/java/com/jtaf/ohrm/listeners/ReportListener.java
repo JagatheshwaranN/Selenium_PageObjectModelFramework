7483d2b5be4e85e5d28688ceee14064cff8c00d0
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-03-08 13:03:35 +0530

    Report Listener Code Updated

--- src/test/java/com/jtaf/ohrm/listeners/ReportListener.java
+++ src/test/java/com/jtaf/ohrm/listeners/ReportListener.java
@@ -15,6 +15,8 @@ import org.testng.Reporter;
 import com.aventstack.extentreports.Status;
 import com.jtaf.ohrm.base.Page;
 import com.jtaf.ohrm.common.ReusableComponent;
+import com.jtaf.ohrm.utils.EmailConfig;
+import com.jtaf.ohrm.utils.EmailTriggerUtil;
 
 public class ReportListener extends ReusableComponent implements ITestListener, ISuiteListener {
 
@@ -22,18 +24,42 @@ public class ReportListener extends ReusableComponent implements ITestListener, 
 
 	public void onTestStart(ITestResult result) {
 
+		test.log(Status.INFO, result.getName().toUpperCase() + " Test Started");
 	}
 
 	public void onTestSuccess(ITestResult result) {
 
+		System.setProperty("org.uncommons.reportng.escape-output", "false");
+		String passTestCaseBase64ScreenShot = "data:image/png;base64,"
+				+ ((TakesScreenshot) Objects.requireNonNull(Page.driver)).getScreenshotAs(OutputType.BASE64);
+		test.log(Status.PASS, result.getName().toUpperCase() + " Test Passed",
+				test.addScreenCaptureFromBase64String(passTestCaseBase64ScreenShot).getModel().getMedia().get(0));
+		String screenToAttach = captureSnapShot();
+		Reporter.log("<br>");
+		Reporter.log(result.getMethod().getMethodName() + " Test Passed..!!");
+		Reporter.log("<br>");
+		Reporter.log("<a target='_blank' href='" + screenToAttach + "'><img src= '" + screenToAttach
+				+ "' height='100' width='100' /></a>");
 	}
 
 	public void onTestFailure(ITestResult result) {
 
+		System.setProperty("org.uncommons.reportng.escape-output", "false");
+		String failTestCaseBase64ScreenShot = "data:image/png;base64,"
+				+ ((TakesScreenshot) Objects.requireNonNull(Page.driver)).getScreenshotAs(OutputType.BASE64);
+		test.log(Status.PASS, result.getName().toUpperCase() + " Test Passed",
+				test.addScreenCaptureFromBase64String(failTestCaseBase64ScreenShot).getModel().getMedia().get(0));
+		String screenToAttach = captureSnapShot();
+		Reporter.log("<br>");
+		Reporter.log(result.getMethod().getMethodName() + " Test Failed..!!");
+		Reporter.log("<br>");
+		Reporter.log("<a target='_blank' href='" + screenToAttach + "'><img src= '" + screenToAttach
+				+ "' height='100' width='100' /></a>");
 	}
 
 	public void onTestSkipped(ITestResult result) {
 
+		test.log(Status.SKIP, result.getName().toUpperCase() + " Test Skipped. At the RunMode is set to N");
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
@@ -46,10 +72,12 @@ public class ReportListener extends ReusableComponent implements ITestListener, 
 
 	public void onStart(ITestContext context) {
 
+		test = report.createTest(context.getName());
 	}
 
 	public void onFinish(ITestContext context) {
 
+		report.flush();
 	}
 
 	public void onStart(ISuite suite) {
@@ -58,5 +86,14 @@ public class ReportListener extends ReusableComponent implements ITestListener, 
 
 	public void onFinish(ISuite suite) {
 
+		EmailTriggerUtil emailTriggerUtil = new EmailTriggerUtil();
+		try {
+			String[] reportPath = getDataFromPropFile("automationReportPath").split("\\$");
+			messageBody = reportPath[0] + InetAddress.getLocalHost().getHostAddress() + reportPath[1];
+			emailTriggerUtil.sendEmail(EmailConfig.mailServer, EmailConfig.from, EmailConfig.to, EmailConfig.subject,
+					messageBody);
+		} catch (UnknownHostException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
